6ffdadd94226f42402fd569a9d6b5896c2db3373
Takashi Yamamiya <tak@metatoys.org>
2010-01-02 10:29:09 -0800

    test for syntactic extensions

--- 3imp.scm
+++ 3imp.scm
@@ -1,37 +1,31 @@
-;; (record (a b c) ls (+ a b c))
-;(define-macro (record vars val . exps)
-;              `(apply (lambda ,vars ,@exps) ,val))
-;(define-macro (record val vars . exps) ; 3impのrecordの文法
-;  `(apply (lambda ,vars ,@exps) ,val))
+;; Chapter 2.1.3 Syntactic Extensions
 
-;(define-syntax record
-;  (syntax-rules ()
-;	((record (var ...) val exp ...)
-;	 (apply (lambda (var ...) exp ...) val))))
-;(define-syntax record
-;  (syntax-rules ()
-;	((record val (var ...) exp ...)
-;	 (apply (lambda (var ...) exp ...) val))))
-
-(use util.match)
-(define record match-let1)
-
-;(record (x y z) '(1 2 3) (list z y x))
-
-;; (record-case exp1
-;;   [key vars exp2..]
-;;   ...
-;;   [else exp3..]
-(define-syntax record-case
-  (syntax-rules (else)
-    ((record-case exp1 [else exp3 exp4 ...]) ; for else
-     (begin exp3 exp4 ...))
-    ((record-case exp1 [key vars exp2 ...])  ; only one case
-     (record vars (cdr exp1) exp2 ...))
-    ((record-case exp1 [key vars exp2 ...] case2 ...) ; more than one case 
-     (let ([r exp1])
-       (if (eq? (car r) (quote key))
-         (record vars (cdr r) exp2 ...)
-         (record-case exp1 case2 ...))))))
+;; recur is used to implement a loop (same as a named let) (p26)
 
 (define recur let)
+
+;; Record binds a set of variables to the element of a list (this list
+;; must contain as many elements as there are variables. (p27)
+;; (this is just another form of "let" expression).
+
+(define-syntax record
+  (syntax-rules ()
+    ((record (var ...) val exp ...)
+     (apply (lambda (var ...) exp ...) val))))
+
+;; The cecord-case syntactic extension is a special purpose
+;; combination of cond and record. It is useful for restructuring a
+;; record based on the "key" that appears as the record's first
+;; element. (p28)
+
+(define-syntax record-case
+  (syntax-rules (else)
+    ((record-case exp1 [else expr3 ...])
+     (begin expr3 ...))
+    ((record-case exp1 [key vars exp2 ...] case2 ...)
+     (let ([r exp1])
+       (if (eq? (car r) (quote key))
+	   (record vars (cdr r) exp2 ...)
+	   (record-case exp1 case2 ...))))
+
+))
